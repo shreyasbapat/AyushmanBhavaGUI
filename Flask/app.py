54c3b1c86e1ac18a465c827465cfe382ae163699
Prabhakar Prasad <prabhakarpd7284@gmail.com>
2018-04-28 15:48:02 +0000

    Add Flask App

---
+++ Flask/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, url_for, render_template
+from flask_restful import Resource, Api
+from data import *
+import requests
+import json
+from bs4 import BeautifulSoup
+import os, subprocess
+
+app = Flask(__name__)
+api = Api(app)
+Articles=Articles()
+Url_for_camera = "http://localhost:8081"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def pag_not_found(e):
+    return render_template('500.html'), 500
+
+@app.route('/')
+def hello():
+    data = {
+      'name' : 'Prabhakar'
+      }
+    return render_template('index.html', **data)
+
+@app.route('/camera')
+def camera():
+   return render_template('camera.html',url_for_camera=Url_for_camera)
+
+@app.route('/cam_off')
+def cam_off():
+   off = "sudo service motor stop"
+   ll = "ls -l"
+   subprocess.call(ll)
+   return
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+
+@app.route('/home')
+def home():
+    ar=requests.get('http://localhost:5001/article')
+    return render_template('home.html', rest_data=ar.text)
+
+
+@app.route('/articles')
+def articles():
+   return render_template('articles.html', articles=Articles)
+
+@app.route('/article')
+def article():
+   return render_template('article.html', articles=Articles)
+
+@app.route('/suggestions',methods=['GET', 'POST'])
+def suggestions():
+    return render_template('suggestions.html')
+
+@app.route('/suggestion')
+def suggestion():
+    text = request.args.get('jsdata')
+
+    suggestions_list = []
+
+    if text:
+        r = requests.get('http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q={}&gl=in'.format(text))
+
+        # r = requests.get('https://www.google.com/search?num=30&source=hp&q={}'.format(text))
+
+        soup = BeautifulSoup(r.content, 'lxml')
+        # soup = BeautifulSoup(html)
+        suggestions = soup.find_all('suggestion')
+        # suggestions = soup.find_all('h3')
+
+        for suggestion in suggestions:
+            suggestions_list.append(suggestion.attrs['data'])
+        # print (suggestions)
+    return render_template('suggestion.html',suggestions=suggestions_list)
+
+class Employees(Resource):
+    def get(self):
+        return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]} 
+
+api.add_resource(Employees, '/employees') # Route_1
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0', port=5001, debug=True)
---
+++ Flask/data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Game Of Thrones',
+            'body':'Story Of Seven Kingdoms',
+            'author':'Prabhakar Prasad',
+            'create-date':'18-04-2018'
+        },
+        {
+            'id':2,
+            'title':'Sherlock Holmes',
+            'body':'Story Of Intelligent Mind',
+            'author':'Prabhakar Prasad',
+            'create-date':'20-04-2018'
+        }
+    ]
+    return articles
