66f2077a88eb410d37d8b718d0d96e45d5f43717
Prabhakar Prasad <prabhakarpd7284@gmail.com>
2018-05-14 20:45:42 +0530

    Remove redundant files

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, url_for, render_template
 from flask_restful import Resource, Api
-from data import *
 import requests
 import json
-from bs4 import BeautifulSoup
 import os
 import subprocess
 from openpyxl import load_workbook
@@ -36,8 +34,6 @@ os.environ['https_proxy'] = "https://10.7.0.1:8080"
 
 app = Flask(__name__)
 api = Api(app)
-Articles=Articles()
-Url_for_camera = "http://localhost:8081"
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -56,14 +52,7 @@ def hello():
 
 @app.route('/camera')
 def camera():
-   return render_template('camera.html',url_for_camera=Url_for_camera)
-
-@app.route('/cam_off')
-def cam_off():
-   off = "sudo service motor stop"
-   ll = "ls -l"
-   subprocess.call(ll)
-   return
+   return render_template('camera.html')
 
 @app.route('/motor1')
 def run_m1():
@@ -88,49 +77,7 @@ def about():
 
 @app.route('/home')
 def home():
-    ar=requests.get('http://localhost:5001/article')
-    return render_template('home.html', rest_data=ar.text)
-
-
-@app.route('/articles')
-def articles():
-   return render_template('articles.html', articles=Articles)
-
-@app.route('/article')
-def article():
-   return render_template('article.html', articles=Articles)
-
-@app.route('/suggestions',methods=['GET', 'POST'])
-def suggestions():
-    return render_template('suggestions.html')
-
-@app.route('/suggestion')
-def suggestion():
-    text = request.args.get('jsdata')
-
-    suggestions_list = []
-
-    if text:
-        r = requests.get('http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q={}&gl=in'.format(text))
-
-        # r = requests.get('https://www.google.com/search?num=30&source=hp&q={}'.format(text))
-
-        soup = BeautifulSoup(r.content, 'lxml')
-        # soup = BeautifulSoup(html)
-        suggestions = soup.find_all('suggestion')
-        # suggestions = soup.find_all('h3')
-
-        for suggestion in suggestions:
-            suggestions_list.append(suggestion.attrs['data'])
-        # print (suggestions)
-    return render_template('suggestion.html',suggestions=suggestions_list)
-
-class Employees(Resource):
-    def get(self):
-        return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]} 
-
-api.add_resource(Employees, '/employees') # Route_1
-
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
